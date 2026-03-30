a72e93166d95d8bb45f9d2c2506072349c0e2d53
junmopark <fierce.unarmed_0l@icloud.com>
2026-03-30 17:15:50 +0900

    feat: 섹션 제작

---
+++ src/app/_components/challenge-section.tsx
@@ -0,0 +1,3 @@
+export default function ChallengeSection() {
+  return <div>ChallengeSection</div>;
+}
---
+++ src/app/_components/shopping-section.tsx
@@ -0,0 +1,3 @@
+export default function ShoppingSection() {
+  return <div>ShoppingSection</div>;
+}
